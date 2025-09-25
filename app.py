6de0632f1fb7ea8e4d1c7ea0b54af8a244d6ac3d
Tuan Minh <tuanminh07@gmail.com>
2025-09-25 15:39:28 +0700

    chinh sua lan 5

--- app.py
+++ app.py
@@ -52,5 +52,7 @@ def clear():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    # Chỉ chạy local (không dùng trên Railway)
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
